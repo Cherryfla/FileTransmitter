fe9110aa69d9e5df7dddb7600a9c6c6b13ca488a
一只鱼笔 <843479000@qq.com>
2019-07-13 08:28:18 +0800

    add filetrans function

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -13,7 +13,7 @@
 #include <string>
 
 #define CMD_PORT 8080
-#define TARN_PORT 8081
+#define TRAN_PORT 8081
 #define BSIZE 1024
 
 using namespace std;
@@ -81,6 +81,105 @@ int DealDir(char *fPath, char *nResult)
         closedir(nDir);
     }
     return 0;
+}
+ssize_t sendfile(int out_fd, int in_fd, off_t * offset, size_t count )
+{
+    off_t orig;
+    char buf[BUF_SIZE];
+    size_t toRead, numRead, numSent, totSent;
+
+    if (offset != NULL) {
+        /* Save current file offset and set offset to value in '*offset' */
+        orig = lseek(in_fd, 0, SEEK_CUR);
+        if (orig == -1)
+            return -1;
+        if (lseek(in_fd, *offset, SEEK_SET) == -1)
+            return -1;
+    }
+
+    totSent = 0;
+    while (count > 0) {
+        toRead = count<BUF_SIZE ? count : BUF_SIZE;
+
+        numRead = read(in_fd, buf, toRead);
+        if (numRead == -1)
+            return -1;
+        if (numRead == 0)
+            break;                      /* EOF */
+
+        numSent = write(out_fd, buf, numRead);
+        if (numSent == -1)
+            return -1;
+        if (numSent == 0) {               /* Should never happen */
+            perror("sendfile: write() transferred 0 bytes");
+            exit(-1);
+        }
+
+        count -= numSent;
+        totSent += numSent;
+    }
+
+    if (offset != NULL) {
+        /* Return updated file offset in '*offset', and reset the file offset
+           to the value it had when we were called. */
+        *offset = lseek(in_fd, 0, SEEK_CUR);
+        if (*offset == -1)
+            return -1;
+        if (lseek(in_fd, orig, SEEK_SET) == -1)
+            return -1;
+    }
+    return totSent;
+}
+void ftp_retr(Command *cmd, State *state)
+{
+
+  if(fork()==0){
+    int connection;
+    int fd;
+    struct stat stat_buf;
+    off_t offset = 0;
+    int sent_total = 0;
+    if(state->logged_in){
+
+      /* Passive mode */
+      if(state->mode == SERVER){
+        if(access(cmd->arg,R_OK)==0 && (fd = open(cmd->arg,O_RDONLY))){
+          fstat(fd,&stat_buf);
+          
+          state->message = "150 Opening BINARY mode data connection.\n";
+          
+          write_state(state);
+          
+          connection = accept_connection(state->sock_pasv);
+          close(state->sock_pasv);
+          if(sent_total = sendfile(connection, fd, &offset, stat_buf.st_size)){
+            
+            if(sent_total != stat_buf.st_size){
+              perror("ftp_retr:sendfile");
+              exit(EXIT_SUCCESS);
+            }
+
+            state->message = "226 File send OK.\n";
+          }else{
+            state->message = "550 Failed to read file.\n";
+          }
+        }else{
+          state->message = "550 Failed to get file\n";
+        }
+      }else{
+        state->message = "550 Please use PASV instead of PORT.\n";
+      }
+    }else{
+      state->message = "530 Please login with USER and PASS.\n";
+    }
+
+    close(fd);
+    close(connection);
+    write_state(state);
+    exit(EXIT_SUCCESS);
+  }
+  state->mode = NORMAL;
+  close(state->sock_pasv);
 }
 int main()
 {
@@ -154,69 +253,65 @@ int main()
             int nByteRead;
             while(nByteRead = read(nConn, nBuff, BSIZE))
             {
-                if(nByteRead < BSIZE)
+                if(nByteRead > BSIZE)
                 {
-                    Command *nCommand = new Command;
-                    nCommand->Init(nBuff);
-                    
-                    if(strncmp(nCommand->GetCommand(), "QUIT", 3) == 0)
+                    cerr<<"server read"<<endl;
+                    continue;
+                }                    
+                Command *nCommand = new Command;
+                nCommand->Init(nBuff);
+                
+                if(strncmp(nCommand->GetCommand(), "QUIT", 3) == 0)
+                {
+                    break;
+                }
+                else if(strncmp(nCommand->GetCommand(), "GET", 3) == 0)
+                {
+                
+                    struct stat nPath;
+                    if(stat(nCommand->GetArg(), &nPath)==0)
                     {
-                        break;
-                    }
-                    else if(strncmp(nCommand->GetCommand(), "GET", 3) == 0)
-                    {
-                   
-                        struct stat nPath;
-                        if(stat(nCommand->GetArg(), &nPath)==0)
+                        if(nPath.st_mode & S_IFREG)
                         {
-                            if(nPath.st_mode & S_IFREG)
+                            cout<<"file\n";
+                        }
+                        else if(nPath.st_mode & S_IFDIR)
+                        {
+                            char nFileList[BSIZE];
+                            memset(nFileList, 0, sizeof(nFileList));
+                            
+                            if(DealDir(nCommand->GetArg(), nFileList) < 0)
                             {
-                                cout<<"file\n";
+                                cerr<<"path error"<<endl;
+                                continue;
                             }
-                            else if(nPath.st_mode & S_IFDIR)
+                            
+                            if(strlen(nFileList) > BSIZE)
                             {
-                                char nFileList[BSIZE];
                                 memset(nFileList, 0, sizeof(nFileList));
-                                
-                                if(DealDir(nCommand->GetArg(), nFileList) < 0)
-                                {
-                                    cerr<<"path error"<<endl;
-                                    continue;
-                                }
-                                
-                                if(strlen(nFileList) > BSIZE)
-                                {
-                                    memset(nFileList, 0, sizeof(nFileList));
-                                    strcat(nFileList, "too many files");
-                                }
-                                write(nConn, nFileList, sizeof(nFileList));
+                                strcat(nFileList, "too many files");
+                            }
+                            write(nConn, nFileList, sizeof(nFileList));
 
-                            }
-                            else
-                            {
-                                printf("not file or dir");
-                            }
                         }
                         else
                         {
-                            char nError[BSIZE] = "Error: file or dir does not exit\n";
-                            write(nConn, nError, sizeof(nError));
+                            printf("not file or dir");
                         }
-                        
                     }
                     else
                     {
-                        char nUsage[BSIZE] = "Usage: GET file/dir,QUIT to quit";
-                        write(nConn, nUsage, sizeof(nUsage));
-                        continue;
+                        char nError[BSIZE] = "Error: file or dir does not exit\n";
+                        write(nConn, nError, sizeof(nError));
                     }
-
-                    delete nCommand;
                 }
                 else
                 {
-                    cerr<<"server read"<<endl;
+                    char nUsage[BSIZE] = "Usage: GET file/dir,QUIT to quit";
+                    write(nConn, nUsage, sizeof(nUsage));
+                    continue;
                 }
+                delete nCommand;
             }
 
         }
